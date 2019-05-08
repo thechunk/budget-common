3a6c1f259f60552f093749f017bb8c38ad0b33f6
Russell Cheung <thechunk@gmail.com>
2019-05-08 19:06:35 +0800

    auth: add google oauth details

---
+++ src/main/kotlin/common/constants/Auth.kt
@@ -0,0 +1,5 @@
+package common.constants
+
+object Auth {
+    const val AUTHORITIES_GOOGLE_SHEETS_WRITE = "google_sheets_write"
+}
---
+++ src/main/kotlin/common/entities/MessageRequest.kt
@@ -0,0 +1,5 @@
+package common.entities
+
+class MessageRequest(
+    val record: BudgetRecord
+)
